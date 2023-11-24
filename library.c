0c5f24dc4e3a50adbc720bf91e712de1f66352c4
Sergio-Bianchi <sergio.bianchi.2007@calvino.edu.it>
2023-11-24 09:35:31 +0100

    Devo un secondo cambiare file, poi faccio un commit decente

--- library.c
+++ library.c
@@ -3,9 +3,28 @@
 #include <stdio.h>
 
 
-#include "library.h"
+// Functions declaration
 
-int main() {
+
+
+
+int libOS = 0; // WiP
+
+
+int titleLenght;
+int authorLenght;
+
+int rawWidth;
+int rawHeight;
+
+
+int intMargin;
+char * textAuthor;
+char * textTile;
+
+
+
+int main(int argc, char * argv[]) {
      /*This function is only for testing purposes*/
 
      testLibrary();
@@ -17,8 +36,21 @@ static void hello(void) {
     printf("Hello, World!\n");
 }
 
-void testLibrary(void) {
-    hello();
+void menu(int OS, int margin, char * title, char * author) {
+
+    //libOS = OS;
+
+    textTile = title;
+
+
+    intMargin = margin;
+    if(OS) {
+        /* In future, I have the idea to implement a Windows version without pipes*/
+        printf("Not implemented yet");
+        return;
+    } else {
+
+    }
 }
 
 
