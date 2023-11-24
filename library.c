78b48910b586eb3301bfcc6dd8ae4bc1401de89a
Sergio-Bianchi <sergio.bianchi.2007@calvino.edu.it>
2023-11-24 07:49:13 +0100

    First commit

---
+++ library.c
@@ -0,0 +1,22 @@
+#include "library.h"
+
+#include <stdio.h>
+
+
+/*int main() {
+    *//* This function is only for testing purposes*//*
+
+
+    return 1;
+}*/
+
+static void hello(void) {
+    printf("Hello, World!\n");
+}
+
+void testLibrary(void) {
+    hello();
+}
+
+
+
---
+++ library.h
@@ -0,0 +1,7 @@
+#ifndef C_LIBMENU_LIBRARY_H
+#define C_LIBMENU_LIBRARY_H
+
+
+void testLibrary(void);
+
+#endif //C_LIBMENU_LIBRARY_H
